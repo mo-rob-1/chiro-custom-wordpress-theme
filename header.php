89a6a3c15cb26c781e56268be0f4350f1acd5fcb
Mohammed Rob <mohammed_rob@hotmail.co.uk>
2020-12-11 11:01:10 +0000

    created navbar for mobile view

--- header.php
+++ header.php
@@ -8,13 +8,15 @@
 
 <header class="header">
 
-    <div class="header__logo"><a class="logo__link" href="<?php echo get_home_url() ?>">Chiro</a></div>
+    <div class="header__logo-and-toggle-container">
+        <div class="header__logo"><a class="logo__link" href="<?php echo get_home_url() ?>">Chiro</a></div>
 
-    <button id="toggle" class="hamburger hamburger--squeeze js-hamburger menu-icon" type="button">
-        <span class="hamburger-box">
-          <span class="hamburger-inner"></span>
-        </span>
-    </button>
+        <button id="toggle" class="hamburger hamburger--squeeze js-hamburger menu-icon" type="button">
+            <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+            </span>
+        </button>
+    </div>
     
     <nav class="header__nav">
             <ul id="nav-panel" class="header__nav-ul">
@@ -23,7 +25,7 @@
                 <li class="header__nav-li"><a href="<?php echo site_url('/services') ?>">Services</a></li>
                 <li class="header__nav-li"><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
                 <li class="header__nav-li"><a href="<?php echo site_url('/contact') ?>">Contact</a></li>
-                <li class="header__nav-li"><a href="<?php echo site_url('/book-appointment') ?>">Book Appointment</a></li>
+                <li class="header__nav-li header__book-appointment"><a href="<?php echo site_url('/book-appointment') ?>">Book Appointment</a></li>
             </ul>
     </nav>
 </header>
