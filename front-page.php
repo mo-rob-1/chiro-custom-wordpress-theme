2491b58ac23e65b29f84c8716fb7aaa66ad3e3d8
Mohammed Rob <mohammed_rob@hotmail.co.uk>
2020-12-15 13:39:52 +0000

    created homepage slideshow - mobile view

--- front-page.php
+++ front-page.php
@@ -1,6 +1,30 @@
 <?php get_header(); ?>
 
-
+<section class="slideshow">
+    <div class="slideshow__container" class="carousel" data-flickity>
+        <div class="slideshow__item-1 carousel-cell">
+            <div class="slideshow__content-wrapper">
+                <h1 class="slideshow__title">Manchester Chiropractic</h1>
+                <p class="slideshow__desc">Specialist, Manchester based chiropractic with years of experience and expertise</p>
+                <a class="slideshow__link" href="<?php echo site_url('/contact') ?>">Contact us</a>
+            </div>
+        </div>
+        <div class="slideshow__item-2 carousel-cell">
+            <div class="slideshow__content-wrapper">
+                <h1 class="slideshow__title">Severe Back Pain?</h1>
+                <p class="slideshow__desc">Book an appointment with one of our expert physiotherapists today and begin the journey to a pain free life</p>
+                <a class="slideshow__link" href="<?php echo site_url('/book-appointment') ?>">Book Appointment</a>
+            </div>
+        </div>
+        <div class="slideshow__item-3 carousel-cell">
+            <div class="slideshow__content-wrapper">
+                <h1 class="slideshow__title">Sports Injury?</h1>
+                <p class="slideshow__desc">No problem. Our experienced professionals will have you back on the field in no time</p>
+                <a class="slideshow__link" href="<?php echo site_url('/services') ?>">View Service</a>
+            </div>
+        </div>
+    </div>
+</section>
  
 <?php get_footer(); ?>
 
--- functions.php
+++ functions.php
@@ -13,9 +13,11 @@ add_action('after_setup_theme', 'chiro_features');
 function chiro_child_theme_files() {
     wp_enqueue_style('google-main-font', 'https://fonts.googleapis.com/css2?family=Open+Sans&family=Signika&display=swap');
     wp_enqueue_style('hamburgers', get_stylesheet_directory_uri() . '/node_modules/hamburgers/dist/hamburgers.css');
+    wp_enqueue_style('flickity', get_stylesheet_directory_uri() . '/node_modules/flickity/dist/flickity.min.css');
     wp_register_script( 'jQuery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', null, null, true );
     wp_enqueue_script('jQuery');
     wp_enqueue_script('script', get_theme_file_uri('/js/script.js'), NULL, '1.0', true);
+    wp_enqueue_script('script-flickity', get_theme_file_uri('/node_modules/flickity/dist/flickity.pkgd.min.js'), NULL, '1.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'chiro_child_theme_files');
