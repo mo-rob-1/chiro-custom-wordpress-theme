cb24b45ecea12b8995093c71dfebcfabab71ede2
Mohammed Rob <mohammed_rob@hotmail.co.uk>
2020-12-22 18:49:13 +0000

    created services section for mobile view

--- archive-wporg_service.php
+++ archive-wporg_service.php
@@ -1,1 +1,31 @@
-eaffa
+<?php
+
+get_header(); ?>
+
+<section class="landing">
+    <h1 class="landing__title"><?php if (is_category()) {
+       single_cat_title();
+    } 
+    if (is_author()) {
+        echo 'Posts by '; the_author() ;
+    }?></h1>
+</section>
+
+<section class="blog">
+<?php
+    while(have_posts()) {
+        the_post(); ?>
+        <div class="blog__post-item">
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('d-m-Y'); ?> in <?php echo get_the_category_list(', '); ?></p>
+        <?php the_excerpt(); ?>
+        <p><a href="<?php the_permalink(); ?>">View Full Post</a></p>
+        </div>
+    <?php }
+    echo paginate_links();
+?>
+</section>
+
+<?php get_footer();
+
+?>
--- front-page.php
+++ front-page.php
@@ -25,6 +25,50 @@
         </div>
     </div>
 </section>
+
+<section class="services">
+
+    <div class="services__container">
+
+    <div class="services__intro-container">
+        <h2 class="services__intro-title">Chiro Services</h2>
+        <p class="services__intro-desc">Here at Veronica physiotherapy clinic we strive to provide you with an exceptional quality of care across all our services which range from, 
+        physiotherapy, ergonomics and more. All therapy services are carried out by experienced and fully trained experts. 
+        To see our full range of therapy services please click on the link below.</p>
+        <a class="services__intro-link" href="<?php echo site_url('/services') ?>">All Services</a>
+    </div>
+
+    <!-- <div class="services__dynamic-container"> -->
+            <?php
+            $args = array(
+                'post_type' => 'post',
+                'post_type' => 'wporg_service',
+                'posts_per_page' => 2
+            );
+
+            $post_query = new WP_Query($args);
+
+            if($post_query->have_posts() ) {
+                while($post_query->have_posts() ) {
+                    $post_query->the_post();
+                    ?>
+                        <div class="services__item">
+                            <div class="services__image-wrapper"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></div>
+                            <div class="services__text-wrapper">
+                                <h2 class="services__heading"><a class="services__heading-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                                <p class="services__desc"><?php echo wp_trim_words(get_the_excerpt(), 17); ?></p>
+                                <p><a class="services__link-read-more" href="<?php the_permalink(); ?>">Read more</a></p>
+                            </div>
+                        </div>
+                    <?php
+                    } wp_reset_postdata();
+                }
+            ?>
+    <!-- </div> -->
+
+    </div>
+
+</section>
  
 <?php get_footer(); ?>
 
